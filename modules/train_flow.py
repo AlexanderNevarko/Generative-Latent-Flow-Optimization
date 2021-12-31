3ab6f4c08719e8b9b702f3b754e01ed47efec7ce
Alexander Nevarko <nevarko.ad@phystech.edu>
2021-12-31 15:52:12 +0300

    upd

--- modules/train_flow.py
+++ modules/train_flow.py
@@ -8,7 +8,7 @@ from torchvision import transforms
 import numpy as np
 
 from .utils import standard_normal_logprob
-from .visualization import visualize_image_grid
+from .visualization import visualize_image_grid, img_side_by_side
 
 
 from tqdm.notebook import tqdm
@@ -17,9 +17,11 @@ from collections import Counter
 
 
 class FlowTrainer():
-    def __init__(self, model, logger=None):
+    def __init__(self, model, logger=None, generator_model=None, basic_model=None):
         self.model = model
         self.logger = logger
+        self.generator = generator_model
+        self.basic = basic_model
         self.device = next(iter(self.model.parameters())).device
     
     def train(self, n_epochs,
@@ -27,15 +29,14 @@ class FlowTrainer():
             #   criterion, 
               optimizer, 
               scheduler=None,
-              generator_model=None,
               exp_name='',
               model_path=''):
         if self.logger is not None:
             self.logger.set_name(exp_name)
-        self.model.train()
         cnt = Counter()
         best_epoch_loss = np.inf
         for epoch in range(n_epochs):
+            self.model.train()
             running_loss = []
             pbar = tqdm(train_loader, leave=True)
             for latents in pbar:
@@ -55,9 +56,18 @@ class FlowTrainer():
             print(f'Average epoch {epoch} train loss: {epoch_loss}')
             if self.logger is not None:
                 self.logger.log_metric(f'Average epoch train loss', epoch_loss, epoch=epoch, step=epoch)
-                if generator_model is not None:
-                    self.logger.log_image(visualize_image_grid(generator_model, inputs=self.model.sample(16)), 
-                                          name=f'Epoch {epoch}', step=epoch)
+                if self.generator is not None:
+                    self.model.eval()
+                    if self.basic is None:
+                        self.logger.log_image(visualize_image_grid(self.generator, 
+                                                                   inputs=self.model.sample(16)), 
+                                              name=f'Epoch {epoch}', step=epoch)
+                    else:
+                        flow_img = self.generator(inputs=self.model.sample(16))
+                        basic_img = self.generator(inputs=self.basic.sample(16))
+                        self.logger.log_image(img_side_by_side(flow_img, basic_img), 
+                                              name=f'Epoch {epoch} flow vs gaussian inference',
+                                              step=epoch)
             if epoch_loss < best_epoch_loss:
                 best_epoch_loss = epoch_loss
                 torch.save(self.model.state_dict(), os.path.join(model_path, f'{exp_name}_model.pth'))
--- modules/visualization.py
+++ modules/visualization.py
@@ -1,6 +1,6 @@
 import torch
 
-from torchvision.utils import make_grid
+from torchvision.utils import make_grid, save_image
 from torchvision import transforms
 
 
@@ -14,10 +14,8 @@ def visualize_image_grid(glo_model, inputs=None):
         random_idx = torch.randint(low=0, high=idx_num, size=(16,), device=glo_model.z.weight.device)
         img = glo_model(idx=random_idx)
     img = img.detach().cpu()
-    grid = make_grid(img, nrow=len(img) // 4)
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-    ])
+    grid = make_grid(img, nrow=len(img) // 4, padding=1)
+    transform = transforms.ToPILImage()
     return transform(grid)
 
 def visualize_paired_results(glo_model, dataloader, img_num=8):
@@ -33,8 +31,8 @@ def visualize_paired_results(glo_model, dataloader, img_num=8):
     for i in idx:
         img.append(dataloader.dataset[i][1].detach().cpu())
     
-    img_grid = make_grid(img, nrow=1)
-    pred_grid = make_grid(preds, nrow=1)
+    img_grid = make_grid(img, nrow=1, padding=1)
+    pred_grid = make_grid(preds, nrow=1, padding=1)
     pairs = torch.empty(2, *img_grid.shape, dtype=torch.float32)
     pairs[0] = pred_grid
     pairs[1] = img_grid
@@ -43,5 +41,25 @@ def visualize_paired_results(glo_model, dataloader, img_num=8):
     transform = transforms.ToPILImage()
     return transform(grid)    
     
-
+def img_side_by_side(img1, img2, inv_transform=None, save_file=None):
+    '''
+    Visualize two pairs of image tensor sets
+    img1, img2: tensors with shape B x C x H x W
+    inv_transform: inverse transform for tensor -> image transformation
+        default: torchvision.transforms.ToPILImage() 
+    save_file: str, file name to save images. If None, don't save images.
+        default: None
+    '''
+    if inv_transform is None:
+        inv_transform = transforms.ToPILImage()
+    img1, img2 = img1.detach().cpu(), img2.detcah().cpu()
+    grid1, grid2 = make_grid(img1, nrow=1, padding=1), make_grid(img2, nrow=1, padding=1)
+    pairs = torch.empty(2, *grid1.shape, ftype=torch.float32)
+    pairs[0], pairs[1] = grid1, grid2
+    grid = make_grid(pairs, nrow=2, padding=1)
+    if save_file is not None:
+        save_image(grid, save_file)
+    return inv_transform(grid)
+    
+    
 
