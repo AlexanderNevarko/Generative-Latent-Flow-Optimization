f1dba2bc29affaa9a676c336b99adbcf073b1b4d
Alexander Nevarko <nevarko.ad@phystech.edu>
2022-02-14 22:36:28 +0300

    Training in one file

---
+++ modules/train_joint.py
@@ -0,0 +1,132 @@
+import os
+import torch
+import torch.nn as nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import numpy as np
+from .visualization import img_raw_by_raw
+from .loss import ValLoss
+
+from tqdm import tqdm
+
+from collections import Counter
+
+def bad_loss(loss):
+    return (loss > 10) or torch.isnan(loss) or torch.isinf(loss)
+
+def train_joint(model, flow, train_loader, 
+                g_optimizer, z_optimizer, 
+                flow_optimizer, g_scheduler, 
+                z_scheduler, flow_scheduler, 
+                criterion, val_loss, cfg):
+    model_path = cfg['model_path']
+    device = cfg['device']
+    n_components = cfg['lat_dim']
+    n_epochs = cfg['n_epochs']
+    alpha = cfg['alpha'] # Factor of flow gradient
+    
+    clipping_value = cfg.get('clipping_value', 1e-4) # for flow gradient clipping
+    img_num = cfg.get('img_num', 16) # number of images to log
+    experiment = cfg.get('experiment', None)
+    exp_name = cfg.get('exp_name', None)
+    
+    if experiment is not None:
+        experiment.set_name(exp_name)
+    cnt = Counter()
+        
+    for epoch in tqdm(range(n_epochs)):
+        flow.train()
+        model.train()
+        gen_running_loss = []
+        flow_running_loss = []
+        for i, (idx, img, _) in enumerate(tqdm(train_loader, leave=False)):
+            idx, img = idx.long().to(device), img.float().to(device)
+            
+            g_optimizer.zero_grad()
+            z_optimizer.zero_grad()
+            flow_optimizer.zero_grad()
+            # Generator forward pass
+            preds = model(idx=idx)
+            loss = criterion(preds, img)
+            loss.backward(retain_graph=True)
+            g_optimizer.step()
+            # Save Z gradient from generator and zero it on Z parameter
+            gen_z_grad = model.z.weight.grad.detach()
+            model.z.weight.grad.zero_()
+            # Flow forward pass
+            noise = torch.randn_like(model.z(idx), device=device).float() * 1e-2 # For flow stability
+            normal_z, log_jac_det = flow(model.z(idx) + noise)
+            flow_loss = 0.5 * torch.sum(normal_z**2, 1) - log_jac_det
+            flow_loss = flow_loss.mean() / n_components
+            if bad_loss(flow_loss):
+                print(f'Bad loss {flow_loss} occured here: epoch - {epoch}, iteration - {i}')
+                z_optimizer.step()
+                continue
+            flow_loss.backward()
+            torch.nn.utils.clip_grad_norm_(flow.parameters(), clipping_value)
+            flow_optimizer.step()
+            
+            # Save Z gradient from flow and zero it on Z parameter
+            flow_z_grad = model.z.weight.grad.detach()
+            model.z.weight.grad.zero_()
+            
+            # Reconstruct the Z gradient summing the two gradients from generator and flow
+            model.z.weight.grad = alpha*flow_z_grad + (1-alpha)*gen_z_grad
+            z_optimizer.step()
+            
+            # Log metrics
+            gen_running_loss.append(loss.item())
+            flow_running_loss.append(flow_loss.item())
+            if experiment is not None:
+                experiment.log_metric(f'Train generator loss', loss.item(), epoch=epoch, step=cnt['train'])
+                experiment.log_metric(f'Train flow loss', flow_loss.item(), epoch=epoch, step=cnt['train'])
+            cnt['train'] += 1
+
+        if g_scheduler is not None:
+            g_scheduler.step()
+        if z_scheduler is not None:
+            z_scheduler.step()
+        if flow_scheduler is not None:
+            flow_scheduler.step()
+
+        # Log metrics and images
+        gen_epoch_loss = np.mean(gen_running_loss)
+        flow_epoch_loss = np.mean(flow_running_loss)
+        print(f'Average generator epoch {epoch} train loss: {gen_epoch_loss}')
+        print(f'Average flow epoch {epoch} train loss: {flow_epoch_loss}')
+        if experiment is not None:
+            experiment.log_metric(f'Average generator epoch train loss', gen_epoch_loss, epoch=epoch, step=epoch)
+            experiment.log_metric(f'Average flow epoch train loss', flow_epoch_loss, epoch=epoch, step=epoch)
+            
+            model.eval()
+            flow.eval()
+            normal_samples = torch.randn(img_num, n_components).to(device)
+            random_idx = torch.randint(low=0, high=len(model.z.weight), size=(img_num,), device=device)
+
+            flow_samples, _ = flow(normal_samples, rev=True)
+            flow_imgs = model(inputs=flow_samples)
+            fit_imgs = model(idx=random_idx)
+            train_imgs = torch.stack([train_loader.dataset[i][1].detach().cpu() for i in random_idx])
+            if len(train_imgs.shape) == 3:
+                train_imgs = train_imgs.unsqueeze(1)
+            experiment.log_image(img_raw_by_raw(train_imgs, fit_imgs, flow_imgs), 
+                                name=f'Epoch {epoch}', step=epoch, )
+                
+            if epoch % 10 == 0:
+                print(f'Calculating FID on epoch {epoch}')
+                # FrEIA package
+                def inputs_generator(size):
+                    normal_sample = torch.randn(size, n_components).to(device)
+                    fake_lats, _ = flow(normal_sample, rev=True)
+                    return fake_lats
+
+                fid, inception_score = val_loss(model, train_loader, inputs_generator)
+                print(f'FID: {fid}, IS: {inception_score}')
+                if np.isnan(fid) or np.isnan(inception_score):
+                    print(f'Bad FID {fid} or IS {inception_score}')
+                    fid = 16.0
+                    inception_score = 2.45
+                experiment.log_metric(f'FID', fid, epoch=epoch, step=epoch)
+                experiment.log_metric(f'IS', inception_score, epoch=epoch, step=epoch)
+                
+    torch.save(model.state_dict(), os.path.join(model_path, f'{exp_name}_generator_model.pth'))
+    torch.save(flow.state_dict(), os.path.join(model_path, f'{exp_name}_flow_model.pth'))
---
+++ train.py
@@ -0,0 +1,133 @@
+import yaml
+import os
+import sys
+import argparse
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam, SparseAdam, SGD, lr_scheduler
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST, CIFAR10
+
+import FrEIA.framework as Ff
+import FrEIA.modules as Fm
+
+from modules.glo_generator import SampleGenerator, GLOGenerator, GLOModel
+from modules.loss import LapLoss, Lap35Loss, ValLoss
+from modules.dataset import IdxDataset
+from modules.train_joint import train_joint
+
+
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def get_dataset(cfg_data):
+    if cfg_data['type'] == 'mnist':
+        dataset = IdxDataset(MNIST(root=cfg_data['root'], train=True, 
+                                   download=True, transform=transforms.ToTensor()))
+    elif cfg_data['type'] == 'cifar10':
+        dataset = IdxDataset(CIFAR10(root=cfg_data['root'], train=True, 
+                                     download=True, transform=transforms.ToTensor()))
+    loader = DataLoader(dataset, batch_size=cfg_data['batch_size'], 
+                        shuffle=True, num_workers=cfg_data['num_workers'], pin_memory=True)
+    sampler_loader = DataLoader(dataset, batch_size=cfg_data['batch_size'], 
+                                shuffle=False, num_workers=cfg_data['num_workers'], pin_memory=True)
+    return loader, sampler_loader
+    
+    
+def get_optimizer(model, cfg_opt):
+    if cfg_opt['type'] == 'adam':
+        return Adam(model.parameters(), lr=cfg_opt['lr'])
+    if cfg_opt['type'] == 'sparse_adam':
+        return SparseAdam(model.parameters(), lr=cfg_opt['lr'])
+    if cfg_opt['type'] == 'sgd':
+        return SGD(model.parameters(), lr=cfg_opt['lr'])
+    else:
+        print(f'Unknown optimizer in config: {cfg_opt["type"]}')
+
+def get_scheduler(opt, cfg_opt):
+    if 'lr_policy' not in cfg_opt:
+        return None
+    if cfg_opt['lr_policy']['type'] == 'step':
+        scheduler = lr_scheduler.StepLR(
+            opt,
+            step_size=cfg_opt['lr_policy']['step_size'],
+            gamma=cfg_opt['lr_policy']['gamma'])
+    elif cfg_opt['lr_policy']['type'] == 'multistep':
+        scheduler = lr_scheduler.MultiStepLR(
+            opt,
+            step_size=cfg_opt['lr_policy']['steps'],
+            gamma=cfg_opt['lr_policy']['gamma'])
+    else:
+        return NotImplementedError('Learning rate policy {} not implemented.'.
+                                   format(cfg_opt['lr_policy']['type']))
+    return scheduler
+
+def get_loss(cfg_loss):
+    if cfg_loss['type'] == 'LapLoss':
+        return LapLoss(**cfg_loss['params'])
+    if cfg_loss['type'] == 'Lap35Loss':
+        return Lap35Loss(**cfg_loss['params'])
+    else:
+        print(f'Unknown loss type in config: {cfg_loss["type"]}')
+
+
+def get_flow(n_components, cfg_flow):
+    def subnet_fc(dims_in, dims_out):
+        middle_dim = cfg_flow['middle_dim']
+        fc1 = nn.Linear(dims_in, middle_dim)
+        torch.nn.init.xavier_normal_(fc1.weight)
+        fc2 = nn.Linear(middle_dim, dims_out)
+        torch.nn.init.zeros_(fc2.weight)
+        return nn.Sequential(fc1, nn.ReLU(), fc2)
+    flow = Ff.SequenceINN(n_components)
+    N = cfg_flow['n_blocks']
+    for k in range(N):
+        if cfg_flow['base_block'] == 'AllInOneBlock':
+            flow.append(Fm.AllInOneBlock, subnet_constructor=subnet_fc, permute_soft=True)
+        else:
+            print(f'Unknown base block in config: {cfg_flow["base_block"]}')
+        if k < N-1:
+            if cfg_flow['transitional_block'] == 'PermuteRandom':
+                flow.append(Fm.PermuteRandom)
+            elif cfg_flow['transitional_block'] == 'OrthogonalTransform':
+                flow.append(Fm.OrthogonalTransform)
+            else:
+                print(f'Unknown transitional block in config: {cfg_flow["transitional_block"]}')
+    return flow
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Train arguments')
+    parser.add_argument('-c', '--cfg', help='Config file path', type=str, default='', required=True)
+    args = parser.parse_args()
+    cfg_file = args.cfg
+    with open(cfg_file, 'r') as file:
+        cfg = yaml.load(file)
+    device = torch.device(cfg['device'])
+    n_components = cfg['lat_dim']
+    bw_method = cfg['bw_method']
+    kwargs = {'cfg': cfg}
+    kwargs['train_loader'], sampler_loader = get_dataset(cfg['data'])
+    sampler = SampleGenerator(sampler_loader, z_dim=n_components, bw_method=bw_method)
+    generator = GLOGenerator(dataloader=kwargs['train_loader'], latent_channels=n_components, **cfg['generator']).to(device)
+    kwargs['model'] = GLOModel(generator, sampler, sparse=True).to(device)
+    kwargs['flow'] = get_flow(n_components, cfg['flow']).to(device)
+    
+    kwargs['g_optimizer'] = get_optimizer(kwargs['model'].generator, cfg['g_optimizer'])
+    kwargs['z_optimizer'] = get_optimizer(kwargs['model'].z, cfg['z_optimizer'])
+    kwargs['flow_optimizer'] = get_optimizer(kwargs['flow'], cfg['flow_optimizer'])
+    kwargs['g_scheduler'] = get_scheduler(kwargs['g_optimizer'], cfg['g_optimizer'])
+    kwargs['z_scheduler'] = get_scheduler(kwargs['z_optimizer'], cfg['z_optimizer'])
+    kwargs['flow_scheduler'] = get_scheduler(kwargs['flow_optimizer'], cfg['flow_optimizer'])
+    kwargs['criterion'] = get_loss(cfg['loss'])
+    kwargs['val_loss'] = ValLoss(device)
+    
+    train_joint(**kwargs)
+    
+    
+
+if __name__ == '__main__':
+    main()
